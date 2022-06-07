0729aa185aabaf2a230b81a68f85af1a2b9ccfc8
蓝田_Loto <shorfng@126.com>
2022-06-07 20:06:48 +0800

    线程 - 创建方式（继承 Thread 类）

---
+++ src/main/java/com/loto/thread/a/thread/ExtendsThread.java
@@ -0,0 +1,21 @@
+package com.loto.thread.a.thread;
+
+/**
+ * Author：蓝田_Loto<p>
+ * Date：2018-12-27 16:26<p>
+ * PageName：ExtendsThread.java<p>
+ * Function：线程 - 创建方式（继承 Thread 类）
+ */
+
+public class ExtendsThread extends Thread {
+    public ExtendsThread() {
+    }
+
+    // 重写 run 方法，完成该线程执行的逻辑
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + ",i=" + i);
+        }
+    }
+}
---
+++ src/main/java/com/loto/thread/a/thread/NewThread.java
@@ -0,0 +1,20 @@
+package com.loto.thread.a.thread;
+
+/**
+ * Author：蓝田_Loto<p>
+ * Date：2018-12-27 16:22<p>
+ * PageName：NewThread.java<p>
+ * Function：创建线程
+ */
+
+public class NewThread {
+    public static void main(String[] args) {
+        // 创建自定义线程对象
+        ExtendsThread thread1 = new ExtendsThread();
+        ExtendsThread thread2 = new ExtendsThread();
+
+        // 开启线程（调用线程的 start 方法）
+        thread1.run();   // 没有开启新线程，在主线程中调用 run 方法
+        thread2.start(); // 开启一个新线程，在新线程中调用 run 方法
+    }
+}
